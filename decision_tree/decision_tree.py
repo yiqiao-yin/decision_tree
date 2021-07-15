b97003cb702f5eadeb51510dfe16ab7666ca501a
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-07-14 22:12:55 -0400

    Update decision_tree.py

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -8,32 +8,48 @@ from sklearn import tree
 from matplotlib.pyplot import figure
 from dtreeviz.trees import dtreeviz
 
+
 def prep_data(X_pd, y_pd):
-    '''Prep data: take two dataframes and convert to numpy'''
+    '''Take two dataframes, and convert to numpy and do train test split'''
+    
     X = X_pd.to_numpy()
     y = y_pd.to_numpy()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=10)
-    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
     
-    return X_train, X_test, y_train, y_test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=.25, random_state=42)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    return X_train, X_test, y_train, y_test 
+  
   
 def fit_decision_tree(X_train, y_train):
     dt = DecisionTreeClassifier()
     dt.fit(X_train, y_train)
+    
     print("Depth: ", dt.get_depth())
     print("Number of leaves: ", dt.get_n_leaves())
+    
     return dt
   
+  
 def visualize_tree(dt, data, type=0):
+    
     if type == 0:
-        fig = figure(figsize=(30,20))
-        _ = tree.plot_tree(dt, feature_names=data.feature_names, class_names=data.target_names, filled=True)
-    if type == 1:
-        viz = dtreeviz(dt, data.data, data.target, target_name='target',
-                       feature_names=data.feature_names, 
-                       class_names=data.target_names.tolist())
+        fig = figure(figsize=(25,20))
+        _ = tree.plot_tree(dt, feature_names=data.feature_names, 
+                           class_names=data.target_names,
+                          filled=True)
         
-def evaluate(dt, X_test, y_test):
-   print("Score: ", dt.score(X_test, y_test))
-   plot_confusion_matrix(dt, X_test, y_test)
+    if type == 1:
+        viz = dtreeviz(dt, data.data, data.target,
+                       target_name='target',
+                       feature_names=data.feature_names, 
+                       class_names=data.target_names.tolist()
+                      )
+        viz.view()
+        
+        
+ def evaluate(dt, X_test, y_test):
+    print("Score: ", dt.score(X_test, y_test))
+    plot_confusion_matrix(dt, X_test, y_test)
+    
+    
