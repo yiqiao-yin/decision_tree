4d2b25f02d8a57f16e463928a329491bb7e8a35c
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-07-14 21:57:25 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='machine_learning_package',
+  author='Yiqiao Yin',
+  version='1.0.0',
+  description="Sample python package",
+  packages=['decision_tree']
+)
